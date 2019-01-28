c03b0c5ef17c76442d4bedc9292fdad97e195d22
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2019-01-28 18:41:17 +0530

    commit before heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/DevConnector",
+  mongoURI: "mongodb://<dbuser>:<dbpassword>@ds213755.mlab.com:13755/devtodev",
   tokenSECRET: "Shippuden"
 };
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("./config/mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const publicPath = path.join(__dirname, "Frontend/public");
 const app = express();
 
 // require Routes
@@ -31,6 +33,12 @@ app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(publicPath));
+  app.get("*", (req, res) => {
+    res.send(path.join(publicPath, "index.html"));
+  });
+}
 // Starting a server
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
